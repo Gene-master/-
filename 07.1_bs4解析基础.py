261ab4580d00cccf21e3bb50cbe2b9861768495b
Gene-master <97927446+Gene-master@users.noreply.github.com>
2022-01-18 12:44:03 +0800

    Delete 07.1_bs4解析基础.py

--- 07.1_bs4解析基础.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-from bs4 import BeautifulSoup
-if __name__ == '__main__':
-    #将本地的html文档中的数据加载到该对象中
-    fp = open('./render.html','r',encoding='utf-8')
-    soup = BeautifulSoup(fp,'lxml')
-    print(soup)
